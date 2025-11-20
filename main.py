c27e9d1c6f33761d7ce63d4819cceb05cffd06d7
Udit Agarwal <udit19199@icloud.com>
2025-11-20 19:53:02 +0530

    chore: Saving changes

--- main.py
+++ main.py
@@ -2,22 +2,24 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 
 def main():
-    print("Starting Asteroids")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill("black")
-    pygame.display.flip()
+
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 
 if __name__ == "__main__":
